97e5284c0096589a1a5962a10595cd0fc01d12cf
rickmoonex <rick@moonen.network>
2024-11-30 23:48:40 +0100

    remove follows

--- modules/nixCats/flake.nix
+++ modules/nixCats/flake.nix
@@ -21,9 +21,7 @@
   description = "A Lua-natic's neovim flake, with extra cats! nixCats!";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixCats.url = "github:BirdeeHub/nixCats-nvim";
-    nixCats.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, nixCats, ... }@inputs: let
