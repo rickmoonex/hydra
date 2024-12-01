a585c12985b610f5df74f33351b3d78eca5867ec
rickmoonex <rick@moonen.network>
2024-12-01 10:34:17 +0100

    lazycats fix

--- modules/nixCats/lua/vj/lazy.lua
+++ modules/nixCats/lua/vj/lazy.lua
@@ -1,9 +1,8 @@
 -- NOTE:
 -- list of nix installed plugins lazy should look for rather than download
 -- can be grabbed straight from require('nixCats').pawsible.allPlugins.{ start, opt }
-local pluginList = {}
 local lazypath = nil -- require('nixCats').pawsible.allPlugins.start["lazy.nvim"]
-require('nixCatsUtils.lazyCat').setup(pluginList, lazypath,{ { import = 'vj.plugins' } }, {
+require('nixCatsUtils.lazyCat').setup(lazypath,{ { import = 'vj.plugins' } }, {
   checker = {
     enabled = true,
     notify = false,
